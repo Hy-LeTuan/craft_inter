ebe57389fd25d587ea48f850b6662ce2fa092b31
Hy-LeTuan <letuanhy1507@gmail.com>
2026-03-21 09:51:55 -0500

    add support for functions and function calls

--- clox/vm.c
+++ clox/vm.c
@@ -10,8 +10,23 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 VM vm;
+
+static Value clockNative(int argCount, Value* args)
+{
+    return NUMBER_VAL((double)clock() / CLOCKS_PER_SEC);
+}
+
+static void defineNative(const char* name, NativeFn function)
+{
+    push(OBJ_VAL(copyString(name, (int)strlen(name))));
+    push(OBJ_VAL(newNative(function)));
+    tableSet(&vm.globals, AS_STRING(vm.stack[0]), vm.stack[1]);
+    pop();
+    pop();
+}
 
 static void resetStack()
 {
@@ -23,6 +38,9 @@ void initVM()
 {
     resetStack();
     initTable(&vm.strings);
+
+    defineNative("clock", clockNative);
+
     initTable(&vm.globals);
     vm.objects = NULL;
 }
@@ -115,6 +133,14 @@ static bool callValue(Value callee, int argCount)
         {
             case OBJ_FUNCTION:
                 return call(AS_FUNCTION(callee), argCount);
+            case OBJ_NATIVE:
+            {
+                NativeFn native = AS_NATIVE(callee);
+                Value result = native(argCount, vm.stackTop - argCount);
+                vm.stackTop -= argCount + 1;
+                push(result);
+                return true;
+            }
             default:
                 // non-callable object
                 break;
